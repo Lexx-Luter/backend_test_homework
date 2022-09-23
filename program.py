c4f443091542994c4acbfa24768bea87bc452d43
Алексей Гаврилов <aeg-lexx@yandex.ru>
2022-09-23 11:11:29 +0400

    First Commit on HW

--- program.py
+++
@@ -1,5 +0,0 @@
-import array
-
-print('Я домашка-')
-print('промакашка')
-print(array.__doc__)
--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\dev\backend_test_homework\venv"
+$env:VIRTUAL_ENV="C:\dev\hw_python_oop-master\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
